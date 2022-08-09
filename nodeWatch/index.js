fe756340549e1903afe41749445e3df9881b52b9
Thangtranse <thangtran.se@gmail.com>
2022-08-09 11:19:38 +0700

    update node watch collection mongodb

--- nodeWatch/index.js
+++ nodeWatch/index.js
@@ -5,16 +5,22 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/usersdb", {
-  useNewUrlParser: true,
-});
+mongoose.connect(
+  "mongodb://root:password123@localhost:27017/usersdb?authSource=admin",
+  {
+    useNewUrlParser: true,
+  }
+);
 
 const db = mongoose.connection;
-
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
   console.log("Connected successfully");
 });
+
+const personSchema = new mongoose.Schema({ name: String });
+const Person = db.model("Person", personSchema);
+Person.watch().on("change", (data) => console.log(new Date(), data));
 
 app.listen(3000, () => {
   console.log("Server is running at port 3000");
