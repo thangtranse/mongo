c42dec35bc80f7f3f6797e6d4b66e879480d1dc9
Thangtranse <thangtran.se@gmail.com>
2022-12-15 21:01:04 +0700

    feat: update Æwatch for change

---
+++ watch-for-changes/README.md
@@ -0,0 +1,15 @@
+# NodeJS
+Project use method Watch of mongo to listen all modified.
+
+# REQ
+
+1. NodeJs
+1. Mongoose
+1. ExpressJs
+
+# REF
+
+1. [mongoosejs connection watch](https://mongoosejs.com/docs/api/connection.html#connection_Connection-watch)
+1. [mongodb docs change stream](https://www.mongodb.com/docs/drivers/node/current/usage-examples/changeStream/)
+1. [fakerjs](https://fakerjs.dev/)
+
---
+++ watch-for-changes/index.js
@@ -0,0 +1,56 @@
+const { faker } = require("@faker-js/faker");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb://root:password123@localhost:27017/mongodb-usersdb?authSource=admin",
+  {
+    useNewUrlParser: true,
+  }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+const personSchema = new mongoose.Schema(
+  {
+    fullname: { type: String, required: true },
+    age: { type: Number, required: true },
+    address: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+const Person = db.model("Person", personSchema);
+Person.watch().on("change", (data) => {
+  console.log(new Date().toLocaleString() + ":", data);
+});
+
+// Tự động tạo dữ liệu sau 5s
+setInterval(() => {
+  const data = new Person({
+    fullname: faker.name.firstName(),
+    age: faker.datatype.number({ max: 100 }),
+    address: faker.address.city(),
+  });
+  data.save();
+}, 5000);
+
+// const changeStream = User.watch();
+// changeStream.on('change', (change) => {
+//     console.log(change); // You could parse out the needed info and send only that data.
+//     io.emit('changeData', change);
+// });
+// io.on('connection', function () {
+//     console.log('connected');
+// });
+
+app.listen(3000, () => {
+  console.log("Server is running at port 3000");
+});
